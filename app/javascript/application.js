dd3530aa5470f9ed8853fe43434bc71c136ce701
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-08-16 18:53:52 +0300

    Implementing a small JavaScript example code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,7 @@
 // Entry point for the build script in your package.json
+const appElement = document.getElementById('app')
+appElement.innerHTML = '<strong>Hello World!</strong>'
+
+module.exports = {
+  foo: 'bar'
+}
