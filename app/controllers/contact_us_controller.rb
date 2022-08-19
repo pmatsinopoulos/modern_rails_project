f617ace89525b7d8d023f6c21ab8fe6a1e796028
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-08-19 09:22:17 +0300

    Turbo example that uses Fetch to get the content of an page visit

---
+++ app/controllers/contact_us_controller.rb
@@ -0,0 +1,4 @@
+class ContactUsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'contact_us/new'
+
   # Defines the root path route ("/")
   root "welcome#index"
 end
